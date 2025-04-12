1660e6893ff19af9962b3bcf76e320750bd32b89
CodeLaMat <eyvaz.alishov@gmail.com>
2025-04-12 19:33:18 +0300

    feat: implement backend structure with user, role, and service management; add authentication and authorization middleware; create routes and controllers for user registration, login, profile management, role management, and service listings

---
+++ apps/backend/dist/src/routes/profileRoutes.js
@@ -0,0 +1,22 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const userController_1 = require("../controllers/userController");
+const multer_1 = __importDefault(require("multer"));
+const authorize_1 = require("../middleware/authorize");
+const authenticateUser_1 = require("../middleware/authenticateUser");
+const client_1 = require("@prisma/client");
+//!!TODO: Add the authorize middleware to the routes that need RBAC
+const router = (0, express_1.Router)();
+const upload = (0, multer_1.default)({
+    storage: multer_1.default.memoryStorage(),
+    limits: { fileSize: 5 * 1024 * 1024 },
+});
+// GET /api/profile/:userId"
+router.get("/profile/:userId", authenticateUser_1.authenticateUser, (0, authorize_1.authorizeMiddleware)(client_1.Action.MANAGE_PROFILE), userController_1.getUserProfile);
+// PATCH /api/profile/updateprofile
+router.patch("/profile/updateprofile", authenticateUser_1.authenticateUser, (0, authorize_1.authorizeMiddleware)(client_1.Action.MANAGE_PROFILE), upload.single("image"), userController_1.updateUserProfile);
+exports.default = router;
---
+++ apps/backend/dist/src/routes/roleRoutes.js
@@ -0,0 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const roleController_1 = require("../controllers/roleController");
+const authenticateUser_1 = require("@/middleware/authenticateUser");
+const authorize_1 = require("@/middleware/authorize");
+const client_1 = require("@prisma/client");
+const router = (0, express_1.Router)();
+// Routes for role management
+router.get("/roles", authenticateUser_1.authenticateUser, (0, authorize_1.authorizeMiddleware)("ALL"), roleController_1.getAllRoles);
+router.post("/roles", authenticateUser_1.authenticateUser, (0, authorize_1.authorizeMiddleware)("ALL"), roleController_1.createRole);
+router.delete("/roles/:id", authenticateUser_1.authenticateUser, (0, authorize_1.authorizeMiddleware)(client_1.Action.ALL), roleController_1.deleteRole);
+// Routes for permission management
+router.get("/permissions", authenticateUser_1.authenticateUser, (0, authorize_1.authorizeMiddleware)(client_1.Action.ALL), roleController_1.getAllPermissions);
+router.post("/permissions", authenticateUser_1.authenticateUser, (0, authorize_1.authorizeMiddleware)(client_1.Action.ALL), roleController_1.createPermission);
+router.delete("/permissions/:id", authenticateUser_1.authenticateUser, (0, authorize_1.authorizeMiddleware)(client_1.Action.ALL), roleController_1.deletePermission);
+// Routes for assigning permissions and updating user role
+router.post("/roles/:roleId/assign-permissions", authenticateUser_1.authenticateUser, (0, authorize_1.authorizeMiddleware)(client_1.Action.ALL), roleController_1.assignPermissionsToRole);
+router.patch("/users/:userId/role", authenticateUser_1.authenticateUser, (0, authorize_1.authorizeMiddleware)(client_1.Action.ALL), roleController_1.updateUserRole);
+exports.default = router;
