5aadde099de15c266cda563c56020250619ac33d
CodeLaMat <eyvaz.alishov@gmail.com>
2025-04-17 16:32:29 +0300

    Remove dist and .turbo folders from backend/frontend and ignore them

--- apps/backend/dist/apps/backend/src/lib/prisma.js
+++
@@ -1,2 +0,0 @@
-import { prisma } from "@repo/db";
-export default prisma;
--- apps/backend/dist/packages/db/src/client.js
+++
@@ -1,6 +0,0 @@
-import { PrismaClient } from "../generated/client";
-const globalForPrisma = globalThis;
-export const prisma = globalForPrisma.prisma ||
-    new PrismaClient({ log: ["query", "info", "warn", "error"] });
-if (process.env.NODE_ENV !== "production")
-    globalForPrisma.prisma = prisma; // Prevent creating a new PrismaClient every time in development
